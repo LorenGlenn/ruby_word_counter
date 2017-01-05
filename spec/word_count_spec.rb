126136ffbdd1d465cd07d87e9dc80a1a9ee3fb43
Loren Glenn <lorencglenn@gmail.com>
2017-01-05 13:42:21 -0800

    add test for partial words

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -11,4 +11,7 @@ describe('String#word_count') do
   it("counts how often a word appears in a sentence with special characters") do
     expect(("DoG!, dOg-").word_count("dOg")).to(eq(2))
   end
+  it("counts how often a word appears in a sentence with special characters") do
+    expect(("DoGgoNe, dogparks").word_count("dOg")).to(eq(2))
+  end
 end
