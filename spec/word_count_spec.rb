c8128885f5230aab47909df943a3b6cb29925d49
Loren Glenn <lorencglenn@gmail.com>
2017-01-05 13:51:29 -0800

    complete index view

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -11,7 +11,7 @@ describe('String#word_count') do
   it("counts how often a word appears in a sentence with special characters") do
     expect(("DoG!, dOg-").word_count("dOg")).to(eq(2))
   end
-  it("counts how often a word appears in a sentence with special characters") do
+  it("counts how often a word appears in a sentence with partial words") do
     expect(("DoGgoNe, dogparks").word_count("dOg")).to(eq(2))
   end
 end
